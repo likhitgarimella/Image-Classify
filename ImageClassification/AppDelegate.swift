d57d0027b29d3c15c512bba4143385837de6387e
Likhit Garimella <likhitgmella@gmail.com>
2021-05-23 09:21:06 +0530

    launch screen time 1s

--- ImageClassification/AppDelegate.swift
+++ ImageClassification/AppDelegate.swift
@@ -11,7 +11,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        // Increase Launch Screen Time
+        Thread.sleep(forTimeInterval: 1)
         return true
+        
     }
     
-}   // #18
+}   // #22
